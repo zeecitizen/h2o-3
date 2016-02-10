a60a64a83d0d3286bafe22144a52deb8f7797267
wendycwong <wendycwong@yahoo.com>
2016-02-10 15:24:18 -0800

    check in change in frame.py to make test pyunit_insert_missing.py work.

--- h2o-py/tests/testdir_munging/pyunit_insert_missing.py
+++ h2o-py/tests/testdir_munging/pyunit_insert_missing.py
@@ -10,7 +10,7 @@ from tests import pyunit_utils
 
 def insert_missing():
     # Connect to a pre-existing cluster
-
+    
 
     data = [[1, 2, 3, 1, 'a', 1, 9],
             [1, 6, 4, 2, 'a', 1, 9],
@@ -21,13 +21,12 @@ def insert_missing():
     h2o_data = h2o.H2OFrame(data)
 
     h2o_data.insert_missing_values(fraction = 0.0)
-    print(h2o_data)
-    num_nas = sum([v.isna().sum() for v in h2o_data])
+    print(h2o_data.ncol)
+    num_nas = sum([h2o_data[c].isna().sum() for c in range(h2o_data.ncol)])
     assert num_nas == 0, "Expected no missing values inserted, but got {0}".format(num_nas)
 
     h2o_data.insert_missing_values(fraction = 1.0)
-    print(h2o_data)
-    num_nas = sum([v.isna().sum() for v in h2o_data])
+    num_nas = sum([h2o_data[c].isna().sum() for c in range(h2o_data.ncol)])
     assert num_nas == h2o_data.nrow*h2o_data.ncol, "Expected all missing values inserted, but got {0}".format(num_nas)
 
 
