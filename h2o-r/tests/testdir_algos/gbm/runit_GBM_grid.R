b7d817acc75655c9167dacd24add3a154571f184
Arno Candel <arno.candel@gmail.com>
2016-02-11 16:44:08 -0800

    HOQE-76: Back-port parameter name change for h2o.grid: sort_order -> decreasing (parts of 6e7233b33b9f3)

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_grid.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_grid.R
@@ -38,8 +38,8 @@ check.gbm.grid <- function() {
   print(gg)
 
   # Test grid sorting
-  ascending = h2o.getGrid(grid_id = gg@grid_id, sort_by =  "mse", sort_order = "asc")
-  descending = h2o.getGrid(grid_id = gg@grid_id, sort_by =  "mse", sort_order = "desc")
+  ascending = h2o.getGrid(grid_id = gg@grid_id, sort_by =  "mse", decreasing = FALSE)
+  descending = h2o.getGrid(grid_id = gg@grid_id, sort_by =  "mse", decreasing = TRUE)
 
   ascending_model_ids <- ascending@model_ids
   descending_model_ids <- descending@model_ids
