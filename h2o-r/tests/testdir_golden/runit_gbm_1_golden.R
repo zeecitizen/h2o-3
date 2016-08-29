6518ed5069094bfa49155e553e33ca32cc570cf0
wendycwong <wendycwong@yahoo.com>
2016-08-29 14:26:31 -0700

    HOQE-54: REGRESSION API V3. Fixed runit test looking for field training_MSE from scoring history.

--- h2o-r/tests/testdir_golden/runit_gbm_1_golden.R
+++ h2o-r/tests/testdir_golden/runit_gbm_1_golden.R
@@ -1,8 +1,6 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
 
-
-
 test.gbmMSEgauss.golden <- function() {
 	
 #Import data: 
@@ -11,24 +9,29 @@ smtreesH2O <- h2o.uploadFile(locate("smalldata/gbm_test/smtrees.csv"), destinati
 smtreesR <- read.csv(locate("smalldata/gbm_test/smtrees.csv"))
 
 Log.info("Test H2O generation of MSE for GBM")
+
+browser()
+
 fith2o <- h2o.gbm(x=c("girth", "height"), y="vol", ntrees=3, max_depth=1, distribution="gaussian", min_rows=2, learn_rate=.1, training_frame=smtreesH2O)
+f_model = fith2o@model
+f_model_history = fith2o@model$scoring_history
 
 #Reported MSE from H2O through R
-err <- as.data.frame(fith2o@model$scoring_history$training_MSE)
+err <- as.data.frame(fith2o@model$scoring_history$training_rmse)    # training_MSE field was replaced with training_lmse
 REPMSE <- err[4,]
 
 #MSE Calculated by hand From H2O predicted values
 pred <- as.data.frame(predict(fith2o, newdata=smtreesH2O))
 diff <- pred-smtreesR[,4]
 diffsq <- diff^2
-EXPMSE <- mean(diffsq)
+EXPMSE <- sqrt(mean(diffsq))
 
 Log.info("Print model MSE... \n")
-Log.info(paste("Length of H2O MSE Vec: ", length(fith2o@model$scoring_history$training_MSE),      "\t\t", "Expected Length   : ", 4))
+Log.info(paste("Length of H2O MSE Vec: ", length(fith2o@model$scoring_history$training_rmse), "\t\t", "Expected Length   : ", 4))
 Log.info(paste("H2O Reported MSE  : ", REPMSE, "\t\t", "R Expected MSE   : ", EXPMSE))
 
 Log.info("Compare model statistics in R to model statistics in H2O")
-expect_equal(length(fith2o@model$scoring_history$training_MSE), 4)
+expect_equal(length(fith2o@model$scoring_history$training_rmse), 4)
 expect_equal(fith2o@model$init_f, mean(smtreesH2O$vol), tolerance=1e-4) ## check the intercept term
 expect_equal(REPMSE, EXPMSE, tolerance=1e-4)
 expect_equal(REPMSE>0, TRUE);
