07b42603ad87a5cfa8e8411351e59fc7f769a4bb
wendycwong <wendycwong@yahoo.com>
2016-09-02 15:38:42 -0700

    HOQE-54 Regression API V3.  Fix grid search problem.

--- h2o-py/h2o/grid/grid_search.py
+++ h2o-py/h2o/grid/grid_search.py
@@ -206,7 +206,8 @@ class H2OGridSearch(object):
     else:                                grid_json = H2OConnection.get_json("Grids/"+grid.dest_key)
 
     self.models = [h2o.get_model(key['name']) for key in grid_json['model_ids']]
-    first_model_json = h2o.api("GET /%d/Models/%s" % (rest_ver or 3, grid_json['model_ids'][0]['name']))['models'][0]
+    #get first model returned in list of models from grid search to get model class (binomial, multinomial, etc)
+    first_model_json = H2OConnection.get_json("Models/"+grid_json['model_ids'][0]['name'], _rest_version=kwargs['_rest_version'])['models'][0]
 
     self._resolve_grid(grid.dest_key, grid_json, first_model_json)
 
