7b541d9f1566725519f36afbaebe25abd5e4c679
wendycwong <wendycwong@yahoo.com>
2016-11-10 16:48:52 -0800

    PUBDEV-3672: PCA stalls.  Changed dataset in junit test.

--- h2o-algos/src/test/java/hex/pca/PCATestMultiNode.java
+++ h2o-algos/src/test/java/hex/pca/PCATestMultiNode.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutionException;
 
 public class PCATestMultiNode extends TestUtil {
   public static final double TOLERANCE = 1e-6;
-  @BeforeClass public static void setup() { stall_till_cloudsize(1); }
+  @BeforeClass public static void setup() { stall_till_cloudsize(2); }
 
 
   // quick fix for MarkC here.  Use GLRM for PCA for now since I fixed it already.
@@ -21,7 +21,7 @@ public class PCATestMultiNode extends TestUtil {
     Frame fr = null, fr2= null;
 
     try {
-      fr = parse_test_file(Key.make("pca_400c_2000R.hex"), "smalldata/pca_test/pca_400c_20000R.csv");
+      fr = parse_test_file(Key.make("SDSS_quasar.hex"), "smalldata/pca_test/SDSS_quasar.txt.zip");
 
       PCAModel.PCAParameters parms = new PCAModel.PCAParameters();
       parms._train = fr._key;
