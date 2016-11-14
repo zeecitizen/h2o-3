ba08297bdf174624d4e66a99788c4cb082a13e56
Navdeep Gill <navdeep@h2o.ai>
2016-11-14 15:36:24 -0800

    Improvements to AstSum rapids function: now it returns a ValFrame of sums per column or row instead of the sum for an entire frame, also added axis parameter, and improved documentation. Added AstSumTest junit, as well.

--- h2o-core/src/main/java/water/rapids/ast/prims/reducers/AstSum.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/reducers/AstSum.java
@@ -5,15 +5,15 @@ import water.fvec.Vec;
 /**
  */
 public class AstSum extends AstRollupOp {
-  public String str() {
-    return "sum";
-  }
+    public String str() {
+        return "sum";
+    }
 
-  public double op(double l, double r) {
-    return l + r;
-  }
+    public double op(double l, double r) {
+        return l + r;
+    }
 
-  public double rup(Vec vec) {
-    return vec.mean() * vec.length();
-  }
+    public double rup(Vec vec) {
+        return vec.mean() * vec.length();
+    }
 }
