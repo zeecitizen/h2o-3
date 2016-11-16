f24784a6d217b654061250c57feb3484533b88c0
wendycwong <wendycwong@yahoo.com>
2016-11-16 09:05:33 -0800

    PUBDEV-3694: errors with PCA on wide data.  Added Erin's test to show failure mode.'

--- h2o-algos/src/test/java/hex/pca/PCATestMultiNode.java
+++ h2o-algos/src/test/java/hex/pca/PCATestMultiNode.java
@@ -1,8 +1,11 @@
 package hex.pca;
 
+import hex.DataInfo;
+import hex.SplitFrame;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import water.DKV;
 import water.Key;
 import water.TestUtil;
 import water.fvec.Frame;
@@ -22,12 +25,12 @@ public class PCATestMultiNode extends TestUtil {
 
     try {
       fr = parse_test_file(Key.make("SDSS_quasar.hex"), "smalldata/pca_test/SDSS_quasar.txt.zip");
-
+//      fr = parse_test_file(Key.make("SDSS_quasar.hex"), "bigdata/laptop/jira/pca_pubdev_3672_400c_20000R.csv.zip");
       PCAModel.PCAParameters parms = new PCAModel.PCAParameters();
       parms._train = fr._key;
       parms._k = 4;
       parms._max_iterations = 1000;
-      parms._pca_method = PCAModel.PCAParameters.Method.GLRM;
+      parms._pca_method = PCAModel.PCAParameters.Method.Randomized;
       parms._use_all_factor_levels = true;
 
       model = new PCA(parms).trainModel().get();
@@ -38,6 +41,54 @@ public class PCATestMultiNode extends TestUtil {
     } finally {
       if( fr  != null ) fr.delete();
       if( fr2 != null ) fr2.delete();
+      if (model != null) model.delete();
+    }
+  }
+
+  @Test
+  public void testMultiNodeErinPCA() throws InterruptedException, ExecutionException {
+    PCAModel model = null;
+    Frame fr = null, fr2= null;
+    Frame tr = null, te= null;
+
+    try {
+      fr = parse_test_file("bigdata/laptop/jira/rotterdam.csv.zip");
+      SplitFrame sf = new SplitFrame(fr,new double[] { 0.5, 0.5 },new Key[] { Key.make("train.hex"), Key.make("test.hex")});
+
+      // Invoke the job
+      sf.exec().get();
+      Key[] ksplits = sf._destination_frames;
+      tr = DKV.get(ksplits[0]).get();
+      te = DKV.get(ksplits[1]).get();
+
+      PCAModel.PCAParameters parms = new PCAModel.PCAParameters();
+      parms._train = ksplits[0];
+      parms._valid = ksplits[1];
+      parms._k = 8;
+      parms._max_iterations = 1000;
+      parms._pca_method = PCAModel.PCAParameters.Method.GramSVD;
+
+      model = new PCA(parms).trainModel().get();  // Erin: does not work.  Arghhh!
+
+      // Done building model; produce a score column with cluster choices
+      fr2 = model.score(te);
+      Assert.assertTrue(model.testJavaScoring(te, fr2, 1e-5));
+
+      // try with Power method
+      parms._pca_method = PCAModel.PCAParameters.Method.Power;
+      parms._k = 20;
+      parms._transform = DataInfo.TransformType.STANDARDIZE;
+      parms._use_all_factor_levels = true;
+
+      model = new PCA(parms).trainModel().get();  // Erin: There is error in thbis one.  Arghhh!
+      // Done building model; produce a score column with cluster choices
+      fr2 = model.score(te);
+      Assert.assertTrue(model.testJavaScoring(te, fr2, 1e-5));
+    } finally {
+      if( fr  != null ) fr.delete();
+      if( fr2 != null ) fr2.delete();
+      if( tr  != null ) tr .delete();
+      if( te  != null ) te .delete();
       if (model != null) model.delete();
     }
   }
